320ab8b0ee9e6b8c14fb4dd87281104d5a66e6e2
Raman Mohammed <RamanMhoammed@hotmail.com>
2022-10-16 20:20:31 +0200

    Add project files.

---
+++ databaskonstruktion_testr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace databaskonstruktion_testr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ databaskonstruktion_testr/Models/KidModel.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace databaskonstruktion_testr.Models
+{
+    public class KidModel
+    {
+        private readonly IConfiguration _configuration;
+        private string _connectionString;
+
+        public KidModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration["ConnectionString"];
+        }
+
+        public DataTable GetAllKids()
+        {
+
+            MySqlConnection dbcon = new MySqlConnection(_connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM KID;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable kidTable = ds.Tables["result"];
+            dbcon.Close();
+
+
+            return kidTable;
+        }
+    }
+}
